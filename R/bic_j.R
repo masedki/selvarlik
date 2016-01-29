d21d16414d7a8d54749614c0c85a3d0dc747ff81
masedki <masedki@gmail.com>
2016-01-29 16:40:17 +0100

    debut à débugger ce soir

--- R/bic_j.R
+++ R/bic_j.R
@@ -1,10 +1,20 @@
 bic_j <- function(j,x,S, both=TRUE)
 {
-  min.model = lm(x[,j] ~ 1., data= as.data.frame(x[,S]))
-  biggest <- formula(lm(x[,j]~.,as.data.frame(x[,S])))
-  if(both)
-    reg.model = step(min.model, direction='both', k = log(nrow(x)), scope=biggest, trace=FALSE)
+  if(length(S)==1)
+  {
+    if(BIC(lm(x[,j]~1.)) < BIC(lm(x[,j]~as.vector(x[,S]))))
+    {return(BIC(lm(x[,j]~1.)))}
+    else
+    {return(BIC(lm(x[,j]~as.vector(x[,S]))))}
+  }
   else
-    reg.model = step(min.model, direction='forward', k = log(nrow(x)), scope=biggest, trace=FALSE)  
-  return(BIC(reg.model))
+  {
+    min.model = lm(x[,j] ~ 1., data=x[,S])
+    biggest <- formula(lm(x[,j]~. ,data=x[,S]))
+    if(both)
+      reg.model = step(min.model, direction='both', k = log(nrow(x)), scope=biggest, trace=FALSE)
+    else
+      reg.model = step(min.model, direction='forward', k = log(nrow(x)), scope=biggest, trace=FALSE)
+    return(BIC(reg.model))
+  }
 }
--- R/selvarlik.R
+++ R/selvarlik.R
@@ -1,11 +1,11 @@
 selvarlik <- function(x, nbcluster)
 {
-order <- matrix(NA, length(nbcluster), ncol(x))  
+order <- matrix(NA, length(nbcluster), ncol(x))
 cp <- 1
-for(g in 1:length(nbcluster))  
+for(g in 1:length(nbcluster))
   order[g,] <- orderlik(x, nbcluster[g])
 
-print(order)
+ #print(order)
  mymodels <- list()
  for(g in 1:length(nbcluster))
    mymodels[[g]] <- varsel(order[g,], x, nbcluster[g])
